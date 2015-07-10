c20a3de348422c86a98b8c3cbfadbe366780b114
Erin Keenan <erinok@gmail.com>
2015-07-10 09:33:01 -0700

    don't pointlessesly use time.Duration

--- main.go
+++ main.go
@@ -7,30 +7,48 @@ import (
 	"io"
 	"os"
 	"strings"
-	"time"
 )
 
-const slop = 500 * time.Millisecond
+type tm int
 
-func parseTime(x string) time.Duration {
+const (
+	millisecond tm = 1
+	second         = 1000 * millisecond
+	minute         = 60 * second
+	hour           = 60 * minute
+)
+
+const slop = 500 * millisecond
+
+func parsetm(x string) tm {
 	// 00:28:48,251
-	var h, m, s, ms time.Duration
+	var h, m, s, ms tm
 	_, err := fmt.Sscanf(x, "%d:%d:%d,%d", &h, &m, &s, &ms)
 	if err != nil {
 		fatal("could not parse time:", err)
 	}
-	return h*time.Hour + m*time.Minute + s*time.Second + ms*time.Millisecond
+	return h*hour + m*minute + s*second + ms*millisecond
 }
 
-func parseTimes(l string) (time.Duration, time.Duration) {
+func parsetms(l string) (tm, tm) {
 	// 00:28:48,251 --> 00:28:50,620 ¶ Komm mal rüber.
 	l = strings.Replace(l, "-->", "¶", -1)
 	parts := strings.Split(l, " ¶ ")
 	if len(parts) != 3 {
 		fatal("line not in expected format: START --> END ¶ comment:", l)
 	}
-	return parseTime(parts[0]), parseTime(parts[1])
+	return parsetm(parts[0]), parsetm(parts[1])
 }
+
+func formattm(t tm) string {
+	h := t / hour
+	m := (t % hour) / minute
+	s := t % minute / second
+	ms := t % second / millisecond
+	return fmt.Sprintf("%02d:%02d:%02d,%03d", h, m, s, ms)
+}
+
+func (t tm) String() string { return formattm(t) }
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
@@ -49,7 +67,10 @@ func main() {
 			}
 			fatal("error reading input:", err)
 		}
-		fmt.Println(parseTimes(l))
+		a, b := parsetms(l)
+		a -= slop
+		b += slop
+		fmt.Println(a, b)
 	}
 }
 
